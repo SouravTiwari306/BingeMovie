8aa642fa363bfc2315f935ab9ef95e664267c9e9
Sourav Kumar <83827683+SouravTiwari306@users.noreply.github.com>
2023-08-06 18:29:13 +0530

    controllers update

---
+++ controllers/user-controllers.js
@@ -0,0 +1,131 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import Booking from "../models/Booking.js";
+export const getAllUsers=async(req,res,next)=>{
+    let users;
+    try{
+        users= await User.find();
+    }catch(err){
+        return console.log(err);
+    }
+
+    if(!users){
+        return res.status(500).json({ message: "Unexpected Error Occuered"});
+    }
+    return res.status(200).json({ users })
+}
+
+
+export const signup =async(req,res,next)=>{
+    const {name,email,password}=req.body;
+    if(!name && name.trim()==="" && !email && email.trim()==="" &&!password && password.trim() ===""){
+        return res.status(422).json({message:"Invalid Inputs"})
+}
+const hashedPassword=bcrypt.hashSync(password);
+let user; 
+try{
+
+    user =new User({name,email, password :hashedPassword});
+ user=await user.save();
+}catch(err){
+    return console.log(err);
+}
+if(!user){
+    res.status(500).json({message:"Unexpected Error Occured"})
+}
+return res.status(201).json({id:user._id});
+
+}
+
+export const updateUser= async(req,res,next)=>{
+    const id= req.params.id;
+    const {name,email,password}=req.body;
+    if(!name && name.trim()==="" && !email && email.trim()==="" &&!password && password.trim() ==="")
+    {
+        return res.status(422).json({message:"Invalid Inputs"})
+    }
+    const hashedPassword=bcrypt.hashSync(password);
+    let user;
+    try{
+user= await User.findByIdAndUpdate(id,{name ,email,password: hashedPassword})
+     }
+    catch (err){
+        return console.log(err);
+    }
+    if(!user){
+        return res.status(500).json({message:"something went wrong"});
+    }
+    res.status(200).json({message: "Updated Sucessfully"});
+}
+
+
+export const deleteUser= async(req,res,next)=>{
+    const id =req.params.id;
+    let user;
+    try{
+        user = await User.findByIdAndRemove(id);
+   
+    }catch(err){
+        return console.log(err);
+    }
+    if(!user){
+        return res.status(500).json({message:"something went wrong"});
+    }
+    res.status(200).json({message: "Deleted Sucessfully"});
+}
+
+
+
+export const loginUser= async(req,res,next)=>{
+    const {email,password}=req.body;
+    if( !email && email.trim()==="" &&!password && password.trim() ==="")
+    {
+        return res.status(422).json({message:"Invalid Inputs"})
+    }
+    let existingUser;
+    try{
+     existingUser =await User.findOne({email});
+
+    }catch(err){
+        return console.log(err);
+    }
+    if(!existingUser){
+        return res.status(404).json({message :"User Not found"});
+    }
+    const isPaswwordCorrect= bcrypt.compareSync(password,existingUser.password);
+    if(!isPaswwordCorrect){
+        return res.status(400).json({message :"Incorrect Password Or Email"});
+    }
+    res.status(200).json({message: "Login Sucessfully",id:existingUser._id});
+}
+
+
+export const  getBookingsOfUser = async(req,res,next)=>{
+    const id= req.params.id;
+    let bookings;
+    try{
+   bookings= await Booking.find({user :id }).populate("movie").populate("user");
+    }catch(err){
+        return console.log(err);
+    }
+    if(!bookings){
+        return res.status(500).json({message :"Unable to get Bookings"});
+    }
+    return res.status(200).json({bookings});
+}
+
+
+export const getUsersById=async(req,res,next)=>{
+    let user;
+    const id= req.params.id;
+    try{
+        user= await User.findById(id)
+    }catch(err){
+        return console.log(err);
+    }
+
+    if(!user){
+        return res.status(500).json({ message: "Unexpected Error Occured"});
+    }
+    return res.status(200).json({ user })
+}
